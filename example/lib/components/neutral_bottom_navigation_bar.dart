977d26166832c640e7a04091299658964cb8bb50
Carlos Costa <carllos.nc@gmail.com>
2025-05-17 02:35:22 -0300

    Add NeutralBottomNavigationBar component

---
+++ example/lib/components/neutral_bottom_navigation_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NeutralBottomNavigationBar extends StatelessWidget {
+  const NeutralBottomNavigationBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      onTap: (int index) {},
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.category_outlined),
+          label: 'Categories',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_cart_outlined),
+          label: 'Cart',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings_outlined),
+          label: 'Settings',
+        ),
+      ],
+    );
+  }
+}
