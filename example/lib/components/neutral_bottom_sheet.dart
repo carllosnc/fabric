98eaa55a1373e28716493c6fb2fe4a6bb17356a4
Carlos Costa <carllos.nc@gmail.com>
2025-05-17 02:35:28 -0300

    Add NeutralBottomSheet component for displaying modal bottom sheets

---
+++ example/lib/components/neutral_bottom_sheet.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class NeutralBottomSheet extends StatelessWidget {
+  const NeutralBottomSheet({super.key});
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.95,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FilledButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Close'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: FilledButton(
+        onPressed: () => _showBottomSheet(context),
+        child: const Text('Open Bottom Sheet'),
+      ),
+    );
+  }
+}
