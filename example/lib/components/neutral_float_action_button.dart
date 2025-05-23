23af02433237fc9612125e64928768a863a01f64
Carlos Costa <carllos.nc@gmail.com>
2025-05-23 00:59:05 -0300

    Add hero tags to FloatingActionButtons in NeutralFloatActionButton

--- example/lib/components/neutral_float_action_button.dart
+++ example/lib/components/neutral_float_action_button.dart
@@ -13,18 +13,22 @@ class NeutralFloatActionButton extends StatelessWidget {
         spacing: 10,
         children: [
           FloatingActionButton.large(
+            heroTag: 'fbt1',
             onPressed: () {},
             child: const Icon(Icons.add),
           ),
           FloatingActionButton(
+            heroTag: 'fbt2',
             onPressed: () {},
             child: const Icon(Icons.add),
           ),
           FloatingActionButton.small(
+            heroTag: 'fbt3',
             onPressed: () {},
             child: const Icon(Icons.add),
           ),
           FloatingActionButton.extended(
+            heroTag: 'fbt4',
             label: const Text('Add'),
             icon: const Icon(Icons.add),
             onPressed: () {},
