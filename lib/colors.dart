3cd6029d01348764d8b4ab27dcbbf9999f5cb2e4
Carlos Costa <carllos.nc@gmail.com>
2025-05-31 11:40:36 -0300

    Refactor neutralColor to use CustomColor class for improved color management

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
-MaterialColor neutralColor = MaterialColor(
+class CustomColor extends ColorSwatch<int> {
+  const CustomColor(super.primary, super.swatch);
+
+  Color get shade50 => this[50]!;
+  Color get shade100 => this[100]!;
+  Color get shade200 => this[200]!;
+  Color get shade300 => this[300]!;
+  Color get shade400 => this[400]!;
+  Color get shade500 => this[500]!;
+  Color get shade600 => this[600]!;
+  Color get shade700 => this[700]!;
+  Color get shade800 => this[800]!;
+  Color get shade900 => this[900]!;
+  Color get shade950 => this[950]!;
+}
+
+CustomColor neutralColor = CustomColor(
   0xFF171717,
   <int, Color>{
     50: Color(0xFFFAFAFA),
@@ -13,5 +29,6 @@ MaterialColor neutralColor = MaterialColor(
     700: Color(0xFF404040),
     800: Color(0xFF262626),
     900: Color(0xFF171717),
+    950: Color(0xFF0A0A0A),
   },
 );
