aacd763fd7a955f0384f0c8c081c0b68bbd215da
Carlos Costa <carllos.nc@gmail.com>
2025-05-31 23:04:34 -0300

    Add zinc CustomColor definition for enhanced color palette

--- lib/colors.dart
+++ lib/colors.dart
@@ -51,3 +51,20 @@ CustomColor stone = CustomColor(
     950: Color(0xFF0C0A09),
   },
 );
+
+CustomColor zinc = CustomColor(
+  0xFF3F3F46,
+  <int, Color>{
+    50: Color(0xFFFAFAFA),
+    100: Color(0xFFF4F4F5),
+    200: Color(0xFFE4E4E7),
+    300: Color(0xFFD4D4D8),
+    400: Color(0xFFA1A1AA),
+    500: Color(0xFF71717A),
+    600: Color(0xFF52525B),
+    700: Color(0xFF3F3F46),
+    800: Color(0xFF27272A),
+    900: Color(0xFF18181B),
+    950: Color(0xFF09090B),
+  },
+);
