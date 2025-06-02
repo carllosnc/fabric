bdcff5155303c2148c310bc3ad0682ea5a9d7372
Carlos Costa <carllos.nc@gmail.com>
2025-06-02 03:59:21 -0300

    Update app.dart to replace 'neutral' package with 'fabric' and adjust theme references

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import './pages/home.dart';
 import './pages/navigation_rail.dart';
-import 'package:neutral/neutral.dart';
+import 'package:fabric/fabric.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,8 +9,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: fabric.getLightTheme(),
-      darkTheme: fabric.getDarkTheme(),
+      theme: fabricConfig.getLightTheme(),
+      darkTheme: fabricConfig.getDarkTheme(),
       debugShowCheckedModeBanner: false,
       home: Home(),
       routes: {
