28a8ffde084717476fa7d3ab76b9ff3965500ce9
Carlos Costa <carllos.nc@gmail.com>
2025-06-02 00:26:57 -0300

    Reorder imports in main.dart and add color definitions for fabricBlue, fabricRed, and fabricGreen in colors.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import './app.dart';
+import 'package:flutter/material.dart';
 import 'package:neutral/neutral.dart';
 
 void main() {
--- lib/colors.dart
+++ lib/colors.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:neutral/fabric_color.dart';
 
-//6 tints steps
-
+//4 tints steps, https://colorkit.co/color-shades-generator/0a0a0a/
 FabricColor fabricGrey = FabricColor(
   0xFF0A0A0A,
   <int, Color>{
@@ -17,5 +16,59 @@ FabricColor fabricGrey = FabricColor(
     800: Color(0xFF2E2E2E),
     900: Color(0xFF1B1B1B),
     950: Color(0xFF0A0A0A),
+  },
+);
+
+//4 tints steps, https://colorkit.co/color-shades-generator/00050d/
+FabricColor fabricBlue = FabricColor(
+  0xFF00050D,
+  <int, Color>{
+    50: Color(0xFFE3E4E6),
+    100: Color(0xFFC7CACD),
+    200: Color(0xFFACB1B5),
+    300: Color(0xFF92989D),
+    400: Color(0xFF798086),
+    500: Color(0xFF616970),
+    600: Color(0xFF49525A),
+    700: Color(0xFF333D45),
+    800: Color(0xFF1E2831),
+    900: Color(0xFF0A151F),
+    950: Color(0xFF00050D),
+  },
+);
+
+//4 tints steps, https://colorkit.co/color-shades-generator/0d0000/
+FabricColor fabricRed = FabricColor(
+  0xFF0D0000,
+  <int, Color>{
+    50: Color(0xFFE6E3E2),
+    100: Color(0xFFCEC8C6),
+    200: Color(0xFFB6ADAB),
+    300: Color(0xFF9F9391),
+    400: Color(0xFF887A77),
+    500: Color(0xFF72615E),
+    600: Color(0xFF5D4A47),
+    700: Color(0xFF483430),
+    800: Color(0xFF331F1B),
+    900: Color(0xFF200B08),
+    950: Color(0xFF0D0000),
+  },
+);
+
+//4 tints steps, https://colorkit.co/color-shades-generator/030d00/
+FabricColor fabricGreen = FabricColor(
+  0xFF030D00,
+  <int, Color>{
+    50: Color(0xFFE4E6E3),
+    100: Color(0xFFCACDC8),
+    200: Color(0xFFB0B5AD),
+    300: Color(0xFF979D94),
+    400: Color(0xFF7E867B),
+    500: Color(0xFF677062),
+    600: Color(0xFF505A4B),
+    700: Color(0xFF3A4535),
+    800: Color(0xFF263120),
+    900: Color(0xFF121E0C),
+    950: Color(0xFF030D00),
   },
 );
