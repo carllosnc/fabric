8714e290e9224f0ac6e22c2ba500a1a048cdb023
Carlos Costa <carllos.nc@gmail.com>
2025-06-29 01:14:33 -0300

    Refactor tests in neutral_test.dart to validate theme and color properties

--- test/neutral_test.dart
+++ test/neutral_test.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fabric/fabric.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('should return a valid theme', () {
+    expect(fabricConfig.getLightTheme(), isA<ThemeData>());
+    expect(fabricConfig.getDarkTheme(), isA<ThemeData>());
+  });
+
+  test('color should return a valid custom swatch', () {
+    expect(fabricConfig.color, isA<FabricSwatch>());
+  });
+
+  test('fontFamily should initialize with null value', () {
+    expect(fabricConfig.fontFamily, isNull);
+  });
 }
